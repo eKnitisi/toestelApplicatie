7750ab1608753f150eb6359356b4bf51c90c457b
Tibe Simons <tibesimons@hotmail.be>
2025-05-24 10:51:47 +0200

    setup voor interfaces

--- flutter_application_intro_mobile/lib/Screens/home_screen.dart
+++ flutter_application_intro_mobile/lib/Screens/home_screen.dart
@@ -15,10 +15,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      /*       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
-      ),
+      ), */
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -53,6 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: () => AuthService.signOut(context),
         tooltip: 'Sign Out',
+        backgroundColor: Colors.red,
         child: const Icon(Icons.logout),
       ),
     );
